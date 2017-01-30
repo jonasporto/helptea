cf40e9407f010d2da32d5d869f9d8c77ab8586f7
Jonas Porto <jporto.contato@hotmail.com>
2017-01-30 01:12:32 -0200

    adding config to addon redisToGo

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,8 @@
-$redis = Redis::Namespace.new(:helptea, :redis => Redis.new)
+
+uri = URI.parse(ENV["REDISTOGO_URL"])
+
+$redis = if Rails.env.production?
+           Redis::Namespace.new(:helptea, Redis.new(:host => uri.host, :port => uri.port, :password => uri.password))
+         else
+           Redis::Namespace.new(:helptea, :redis => Redis.new)
+         end

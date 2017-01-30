903626128756a78846cbf426845e39b85f67f5d7
Jonas Porto <jporto.contato@hotmail.com>
2017-01-30 09:18:23 -0200

    moving from redisToGo to heroku-redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,8 @@
 $redis = if Rails.env.production?
-           uri = URI.parse(ENV["REDISTOGO_URL"])
+           uri = URI.parse(ENV["REDIS_URL"])
            Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
          else
            Redis.new
          end
 
-#$redis = Redis::Namespace.new(:helptea, :redis => $redis)
+$redis = Redis::Namespace.new(:helptea, :redis => $redis)
